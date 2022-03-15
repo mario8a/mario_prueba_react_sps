66805fad78e8aec6f3ba9497256a1de3acd421fa
mario8a <mario8ato@hotmail.com>
2022-03-14 23:10:31 -0600

    Show first user from api

--- usersMobile/src/screens/HomeScreen.tsx
+++ usersMobile/src/screens/HomeScreen.tsx
@@ -1,10 +1,38 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, Text, View } from 'react-native';
+import usersAPI from '../api/usersAPI';
+import { UsersDetail, UsersResponse } from '../interfaces/usersInterface';
 
 export const HomeScreen = () => {
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [users, setUsers] = useState<UsersDetail[]>([]);
+
+  const getUsers = async () => {
+    const response = await usersAPI.get<UsersResponse>('/users');
+    const { data } = response.data;
+    setUsers(data);
+
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <View
+        style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+      >
+        <ActivityIndicator color="blue" size={50} />
+      </View>
+    );
+  }
+
   return (
     <View>
-      <Text>Home screen</Text>
+      <Text>{users[0].first_name}</Text>
     </View>
   );
 };
