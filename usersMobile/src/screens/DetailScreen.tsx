d43686c22e52f8f5d924f23035addebea3545de4
mario8a <mario8ato@hotmail.com>
2022-03-14 23:39:30 -0600

    Detail screen styles

--- usersMobile/src/screens/DetailScreen.tsx
+++ usersMobile/src/screens/DetailScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 import { RootStackParams } from '../navigation/Navigation';
 
 interface Props extends StackScreenProps<RootStackParams,'DetailScreen'>{}
@@ -7,11 +7,86 @@ interface Props extends StackScreenProps<RootStackParams,'DetailScreen'>{}
 export const DetailScreen = ({ route }: Props) => {
 
   const user = route.params;
-  console.log(user);
 
   return (
-    <View>
-      <Text>Detail screen</Text>
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <View style={styles.avatarContainer}>
+        <Image
+            source={{uri: user.avatar}}
+            style={{height: '100%', width: '100%', borderRadius: 60}}
+          />
+        </View>
+
+        <View style={styles.textContainer}>
+          <Text style={styles.textName}>{user.first_name}</Text>
+          <Text style={styles.textEmail}>{user.email}</Text>
+          <TouchableOpacity style={styles.btnMessage}>
+            <Text style={{color: 'white', fontWeight: 'bold'}}>
+              Send me a message
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 };
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#c6c6c6',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  imageContainer: {
+    height: 340,
+    width: 340,
+    backgroundColor: 'white',
+    borderRadius: 21,
+    elevation: 3,
+  },
+  avatarContainer: {
+    height: 120,
+    width: 120,
+    alignSelf: 'center',
+    position: 'absolute',
+    top: -50,
+  },
+  textContainer: {
+    height: '90%',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textName: {
+    padding: 10,
+    alignSelf: 'center',
+    fontSize: 30,
+    lineHeight: 26,
+    fontWeight: 'bold',
+  },
+  textEmail: {
+    fontSize: 15,
+    lineHeight: 26,
+  },
+  btnMessage: {
+    height: 35,
+    width: 200,
+    borderRadius: 40,
+    backgroundColor: '#5458F7',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 15,
+  },
+  backBtn: {
+    position: 'absolute',
+    elevation: 9,
+    top: 30,
+    left: 20,
+    padding: 6,
+    backgroundColor: 'white',
+    borderRadius: 15,
+  },
+});
